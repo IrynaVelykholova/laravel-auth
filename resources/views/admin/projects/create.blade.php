d34d48e3df46c575bb101e54a4a71913460955f8
IrynaVelykholova <irynavely93@gmail.com>
2023-10-10 17:27:33 +0200

    + error

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -7,11 +7,31 @@
 
     <form action="{{ route('admin.projects.store') }}" method="POST">
         @csrf()
+        {{-- titolo --}}
+        <div class="mb-3">
+            <label class="form-label">Titolo</label><input type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}">
+            @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
 
-        <div class="mb-3"><label class="form-label">Titolo</label><input type="text" class="form-control" name="title"></div>
-        <div class="mb-3"><label class="form-label">Immagine</label><input type="text" class="form-control" name="image"></div>
-        <div class="mb-3"><label class="form-label">Descrizione</label><textarea class="form-control" name="description"></textarea></div>
+        {{-- immagine --}}
+        <div class="mb-3">
+            <label class="form-label">Immagine</label><input type="text" class="form-control @error('image') is-invalid @enderror" name="image">
+            @error('image')
+                    <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
 
+        {{-- descrizione --}}
+        <div class="mb-3">
+            <label class="form-label @error('description') is-invalid @enderror">Description</label>
+                <textarea class="form-control" name="description">{{ old('description') }}</textarea>
+                @error('description')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+        </div>
+        
         <button class="btn btn-primary">Crea</button>
     </form>
 </div>
